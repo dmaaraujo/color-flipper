886b1ccf9b4100f56988a3d6a5bdf5a78303798a
Daddio <matheus.daddio12@gmail.com>
2025-08-29 08:27:27 -0300

    Adicionando a funcionalidade principal à página inicial

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function(){
+// pegar um número aleatótio entre 0 - 3 colors[3]
+    const randomNumber = getRandomNumeber();
+    console.log(randomNumber)
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumeber(){
+    return Math.floor(Math.random() * colors.length);
+}
